1283ea81260f56cd1027f2e4fac841fccf20c338
zowow <79232193+zowow@users.noreply.github.com>
2021-03-16 18:40:15 +0800

    Initial Commit

---
+++ BasicGeometryProgram.java
@@ -0,0 +1,15 @@
+public class BasicGeometryProgram {
+
+    
+    public static void main(String[] args) {
+        
+         Formulas fmla = new Formulas();
+
+    }
+    
+}
+
+class Formulas{
+    
+    
+}
